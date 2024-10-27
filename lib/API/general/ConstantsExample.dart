226eb0d663a96eae971298b21a2bb3ff364bd0bf
Andrei137 <andrei.fabian188@gmail.com>
2024-10-27 15:00:09 +0200

    added .env.example

--- lib/API/general/ConstantsExample.dart
+++
@@ -1,9 +0,0 @@
-// Note: remove Example from the file's name
-
-// Guide for IGDB: https://api-docs.igdb.com/#getting-started
-
-const String clientIdIGDB = "YOUR_IGDB_CLIENT_ID";
-const String clientSecretIGDB = "YOUR_IGDB_CLIENT_SECRET";
-
-const String urlSupabase = 'YOUR_URL_SUPABASE';
-const String anonKeySupabase = 'YOUR_ANON_KEY_SUPABASE';
