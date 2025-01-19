e4f6209bd9b3cb82f23c5af9a5b07c134d55fc17
Ilie Dumitru <iliemity@yahoo.com>
2025-01-19 15:52:53 +0200

    Tests fix MediaGenre

--- test/resources/media_genre_test.dart
+++ test/resources/media_genre_test.dart
@@ -1,16 +1,15 @@
 import 'dart:core';
 import '../general/resource_test.dart';
-import 'package:mediamaster/Models/media_user_genre.dart';
+import 'package:mediamaster/Models/media_genre.dart';
 import 'package:mediamaster/Services/genre_service.dart';
-import 'package:mediamaster/Services/media_user_genre_service.dart';
+import 'package:mediamaster/Services/media_genre_service.dart';
 
 void main() async {
   Map<String, dynamic> genre = {
     'name': 'test'
   };
-  MediaUserGenre dummy = MediaUserGenre(
+  MediaGenre dummy = MediaGenre(
     mediaId: 1,
-    userId: '',
     genreId: await getValidId(
       service: GenreService.instance,
       backup : genre
@@ -18,9 +17,8 @@ void main() async {
   );
 
   await runService(
-    service    : MediaUserGenreService.instance,
+    service    : MediaGenreService.instance,
     dummyItem  : dummy,
     tables     : ['media', 'genre'],
-    authNeeded : true,
   );
 }
